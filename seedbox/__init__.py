73043654af57b089672ba76c6dd962d6f685a780
WTFox <Anthonyfox1988@Gmail.com>
2017-01-22 13:51:53 -0600

    Validate OS

--- seedbox/__init__.py
+++ seedbox/__init__.py
@@ -2,5 +2,10 @@ __title__ = 'seedbox'
 __version__ = '0.4'
 __author__ = 'Anthony Fox'
 
+import platform
+
+if platform.system() == 'Windows':
+    raise OSError("This is not compatible with windows.")
+
 from .api import cli
 
